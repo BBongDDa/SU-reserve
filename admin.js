e1cab94649ddf8e07c5af1882b80a7b806f2f197
BBongDDa <shsiskd@gmail.com>
2020-07-30 23:01:08 +0900

    기본템플릿 완성 / 반응형 수정 / js 추가

---
+++ admin.js
@@ -0,0 +1,40 @@
+const number = document.getElementById("number");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+
+const number2 = document.getElementById("number2");
+const increase2 = document.getElementById("increase2");
+const decrease2 = document.getElementById("decrease2");
+
+increase.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  number.innerText = current + 1;
+};
+
+decrease.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  if (current > 0)
+  {
+  number.innerText = current - 1;
+  }
+  else{
+      return 0;
+  }
+};
+
+increase2.onclick = () => {
+    const current = parseInt(number2.innerText, 10);
+    number2.innerText = current + 1;
+  };
+  
+  decrease2.onclick = () => {
+    const current = parseInt(number2.innerText, 10);
+    if (current > 0)
+    {
+    number2.innerText = current - 1;
+    }
+    else{
+        return 0;
+    }
+  };
+  
